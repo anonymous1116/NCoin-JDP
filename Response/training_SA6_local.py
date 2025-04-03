d40116b360fe0665deb1d2c469c5652ac8d77288
anonymous1116 <hhyun116@gmail.com>
2025-04-03 13:06:26 -0400

    SA5

--- Response/training_SA6_local.py
+++ Response/training_SA6_local.py
@@ -7,7 +7,7 @@ from module import FL_Net2
 from sbi.utils import BoxUniform
 
 from NCoinJDP import NCoinJDP_train, ABC_rej
-from simulator import Simulators, PBJD_theta_log_transform, PBJD_theta_exp_transform, PBJD_truncated_priors2
+from simulator import Simulators, PBJD_theta_exp_transform, PBJD_theta_log_transform, PBJD_truncated_priors2
 #from utils.batch_process import resid_chunk_process
 
 # Set the default device based on availability
@@ -22,14 +22,17 @@ def main(args):
     delta = 1
     
     test_save_name = 'Robustness/RDA_data.pt'
-    x0= torch.load(test_save_name)
+    test_data= torch.load(test_save_name)
+    x0=test_data[args.x0_ind]
+    x0 = x0.type(torch.float32)
+
+
     simulators = Simulators("PBJD_summary", n = n, delta = delta)
     
-    
-    L = args.num_training
     param = [[-0.01, 0.02], [100], [0.05, 2], [0.05,2], [1/100], [1/100]] 
     trunc = [[-0.01, 0.02], [1e-5, 1e-2], [0.05, 2], [0.05, 2], [10, 300], [10, 300] ]
-    theta = PBJD_truncated_priors2(L, param, trunc)
+    
+    theta = PBJD_truncated_priors2(args.num_training, param, trunc)
     print(f"Prior generated", flush=True)
     
     X = simulators(theta)
@@ -38,35 +41,44 @@ def main(args):
     D_in, D_out, Hs = X.size(1), theta.size(1), args.layer_len
 
     #cases = args.priors[:,2]
-    output_dir = f"../../depot_hyun/hyun/NCoinJDP/{args.experiment}/{args.task}/J_{int(args.num_training/1000)}"
+    output_dir = f"../../depot_hyun/hyun/NCoinJDP/{args.experiment}/{args.task}/J_{int(args.num_training/1000)}/C{args.x0_ind}"
     
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
         print(f"Directory '{output_dir}' created.")
     else:
         print(f"Directory '{output_dir}' already exists.")
-    
-    theta_transform = PBJD_theta_log_transform(theta)
 
-    a = torch.quantile(X, .001, 0)
+    x0 = torch.reshape(x0, (1, x0.size(0)))
+    x0 = simulators.PBJD_summary(x0)
+
+    tol = args.tol
+    print(f"ABC_rej start", flush=True)
+    
+    X_new, theta_new = ABC_rej(x0, X, theta, tol = tol, device = device)
+    
+    
+    theta_transform = PBJD_theta_log_transform(theta_new)
+    a = torch.quantile(X_new, .001, 0)
     a = torch.reshape(a, (1, a.size()[0]))
-    b = torch.quantile(X, .999, 0)
+    b = torch.quantile(X_new, .999, 0)
     b = torch.reshape(b, (1, b.size()[0]))
 
-    X_new = torch.clone((X - a) / (b - a))    
+    X_new = torch.clone((X_new - a) / (b - a))    
     x0 = torch.clone((x0 - a) / (b - a))
 
     print(f"training start", flush=True)
     net = FL_Net2(D_in, D_out, H=Hs, H2=Hs, H3=Hs).to(device)
     val_batch = 10000
-    tmp, _ = NCoinJDP_train(X_new, theta_transform, net, device=device, N_EPOCHS=args.N_EPOCHS, val_batch = val_batch, l2 = "True")
+    tmp, _ = NCoinJDP_train(X_new, theta_transform, net, device=device, N_EPOCHS=args.N_EPOCHS, val_batch = val_batch)
     net.load_state_dict(tmp)
     net.eval()
     net.to("cpu")
 
     print(f"saving", flush=True)
-    torch.save([PBJD_theta_exp_transform(net(x0).detach()),a,b], f"{output_dir}/mean_{args.seed}.pt")
-    print(f"{args.experiment}, {args.task}, seed {args.seed}, priors {args.priors}, x0_ind done", flush=True)
+    torch.save([net(x0).detach(),a,b], f"{output_dir}/local_{args.seed}.pt")
+    print(f"{args.experiment}, {args.task}, seed {args.seed}, priors {args.priors}, x0_ind, {args.x0_ind} done", flush=True)
+    
     
 def get_args():
     parser = argparse.ArgumentParser(description="Run simulation with customizable parameters.")
@@ -82,6 +94,10 @@ def get_args():
                         help = "See number (default: 1)")
     parser.add_argument("--layer_len", type = int, default = 256,
                         help = "layer length of FL network (default: 256)")
+    parser.add_argument("--tol", type = float, default = 0.05,
+                        help = "Tolerance value")
+    parser.add_argument("--x0_ind", type = int, default = 0,
+                        help = "x0_ind")
     return parser.parse_args()
 
 
