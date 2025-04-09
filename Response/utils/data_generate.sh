8edda0536c4f14f3d50b1e1e0e49eb85387df409
anonymous1116 <hhyun116@gmail.com>
2025-04-09 00:43:00 -0400

    SA6_priors

--- Response/utils/data_generate.sh
+++ Response/utils/data_generate.sh
@@ -3,7 +3,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
 #SBATCH --time=00:30:00
-#SBATCH --account=standby
+#SBATCH --account=debug
 #SBATCH --gpus-per-node=1
 #SBATCH --nodes=1
 #SBATCH --array=1
