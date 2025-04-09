c3a79ab11c4dbd2fb44eecc340645bdf71c49022
anonymous1116 <hhyun116@gmail.com>
2025-04-09 10:53:06 -0400

    SA6_priors

--- Response/utils/data_generate.sh
+++ Response/utils/data_generate.sh
@@ -6,7 +6,7 @@
 #SBATCH --account=standby
 #SBATCH --gpus-per-node=1
 #SBATCH --nodes=1
-#SBATCH --array=1000-1999%20
+#SBATCH --array=0-999%20
 #SBATCH --output=output_log/output_log_%A_%a.out
 #SBATCH --error=error_log/error_log_%A_%a.txt
 
@@ -26,7 +26,7 @@ cd $SLURM_SUBMIT_DIR
 # Calculate seed and prior
 seed=$((SLURM_ARRAY_TASK_ID % 100 + 1))
 prior_index=$((SLURM_ARRAY_TASK_ID / 100))
-priors_list=("P1_0" "P1_1" "P1_2" "P1_3" "P1_4" "P2_0" "P2_1" "P2_2" "P2_3" "P2_4" "P3_0" "P3_1" "P3_2" "P3_3" "P3_4" "P4_0" "P4_1" "P4_2" "P4_3" "P4_4")
+priors_list=("P3_0" "P3_1" "P3_2" "P3_3" "P3_4" "P4_0" "P4_1" "P4_2" "P4_3" "P4_4")
 prior=${priors_list[$prior_index]}
 
 # Run the Python script with the specified N_EPOCHS value
