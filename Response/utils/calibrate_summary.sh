7cc882b189a136273e87c10fa81984824fd516f8
anonymous1116 <hhyun116@gmail.com>
2025-04-10 16:02:17 -0400

    SA6_priors

--- Response/utils/calibrate_summary.sh
+++ Response/utils/calibrate_summary.sh
@@ -6,35 +6,29 @@
 #SBATCH --account=standby
 #SBATCH --gpus-per-node=1
 #SBATCH --nodes=1
-#SBATCH --array=0-3
+#SBATCH --array=1-9
 #SBATCH --output=output_log/output_log_%A_%a.out
-#SBATCH --error=error._log/error_log_%A_%a.txt
+#SBATCH --error=error_log/error_log_%A_%a.txt
 
-# Create the output_log directory if it doesn't exist
+# Create output directories
 mkdir -p output_log
 mkdir -p error_log
 
-# Load the required Python environment
+# Load environment
 module load conda
 conda activate NABC
 
-# Change to the directory where the job was submitted from
-SLURM_SUBMIT_DIR=/home/hyun18/NCoin-JDP/Response
-cd $SLURM_SUBMIT_DIR
+# Move to working directory
+cd /home/hyun18/NCoin-JDP/Response
 
+# Set fixed prior and get x0_ind from SLURM array index
+prior="P1_0"
+x0_ind=$SLURM_ARRAY_TASK_ID
 
-# Calculate seed and prior
-prior_index=$((SLURM_ARRAY_TASK_ID))
-#priors_list=("P1_0" "P1_1" "P1_2" "P1_3" "P1_4" "P2_0" "P2_1" "P2_2" "P2_3" "P2_4" "P3_0" "P3_1" "P3_2" "P3_3" "P3_4" "P4_0" "P4_1" "P4_2" "P4_3" "P4_4")
-priors_list=("P1_4" "P2_4" "P3_4" "P4_4")
-
-prior=${priors_list[$prior_index]}
-
-# Run the Python script with the specified N_EPOCHS value
-
-echo "Running with prior=$prior"
+echo "Running with prior=$prior and x0_ind=$x0_ind"
 
 python utils/calibrate_summary.py \
-  --priors "$prior"
+  --priors "$prior" \
+  --x0_ind "$x0_ind"
 
-echo "Running finished with prior=$prior"
+echo "Finished with prior=$prior and x0_ind=$x0_ind"
